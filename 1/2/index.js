bec81c7771aecbf2a8e2f00fa922887ed098e99c
pataar <pietering1@gmail.com>
2020-12-01 19:55:47 +0100

    Test

--- 1/2/index.js
+++ 1/2/index.js
@@ -4,10 +4,10 @@ module.exports = () => {
 
 	for (let i = 0; i < input.length; i++) {
 		for (let j = 0; j < input.length; j++) {
-			for (let n = 0; n < input.length; n++) {
+			for (let k = 0; k < input.length; k++) {
 				const a = input[i];
 				const o = input[j];
-				const c = input[n];
+				const c = input[k];
 				if (a + o + c === 2020) {
 					return a * o * c;
 				}
