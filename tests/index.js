16e6d2408f43d0a81f7bad64cdb0f691db5f0bb5
pataar <pietering1@gmail.com>
2020-12-01 18:59:38 +0100

    Make it easier to duplicate tests

--- tests/index.js
+++ tests/index.js
@@ -4,9 +4,15 @@ const d1p1 = require("../1/1");
 const d1p2 = require("../1/2");
 
 test("day 1 | puzzle 1", t => {
-	t.equal(d1p1(), 793524, "answer should be 793524");
+	const expected = 793524;
+	const puzzle = d1p1;
+
+	t.equal(puzzle(), expected, `answer should be ${expected}`);
 });
 
 test("day 1 | puzzle 2", t => {
-	t.equal(d1p2(), 61515678, "answer should be 61515678");
+	const expected = 61515678;
+	const puzzle = d1p2;
+
+	t.equal(puzzle(), expected, `answer should be ${expected}`);
 });
